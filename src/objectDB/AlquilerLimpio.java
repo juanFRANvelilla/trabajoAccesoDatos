8316dc8a156a1e9ac591dfa60568f6c2387e7ebc
juanFRANvelilla <juanfran01velilla@gmail.com>
2024-03-25 19:07:18 +0100

    agregar copia de alquiler sin dom xax

---
+++ src/objectDB/AlquilerLimpio.java
@@ -0,0 +1,72 @@
+//package objectDB;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Alquiler {
+//    @Id
+//    int id;
+//    String nombreInquilino;
+//    String direccionPiso;
+//    Double importeAlquiler;
+//    int duracionContrato;
+//
+//    public Alquiler(String nombreInquilino, String direccionPiso, Double importeAlquiler, int duracionContrato) {
+//        this.nombreInquilino = nombreInquilino;
+//        this.direccionPiso = direccionPiso;
+//        this.importeAlquiler = importeAlquiler;
+//        this.duracionContrato = duracionContrato;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getNombreInquilino() {
+//        return nombreInquilino;
+//    }
+//
+//    public void setNombreInquilino(String nombreInquilino) {
+//        this.nombreInquilino = nombreInquilino;
+//    }
+//
+//    public String getDireccionPiso() {
+//        return direccionPiso;
+//    }
+//
+//    public void setDireccionPiso(String direccionPiso) {
+//        this.direccionPiso = direccionPiso;
+//    }
+//
+//    public Double getImporteAlquiler() {
+//        return importeAlquiler;
+//    }
+//
+//    public void setImporteAlquiler(Double importeAlquiler) {
+//        this.importeAlquiler = importeAlquiler;
+//    }
+//
+//    public int getDuracionContrato() {
+//        return duracionContrato;
+//    }
+//
+//    public void setDuracionContrato(int duracionContrato) {
+//        this.duracionContrato = duracionContrato;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ALQUILER {" +
+//                "id=" + id +
+//                ", nombreInquilino='" + nombreInquilino + '\'' +
+//                ", direccionPiso='" + direccionPiso + '\'' +
+//                ", importeAlquiler=" + importeAlquiler +
+//                ", duracionContrato=" + duracionContrato +
+//                '}';
+//    }
+//}
